52226d258533156dfaa9001352c45033031247fa
ThiagoPerdigao <thiagovitor299@gmail.com>
2024-06-23 15:19:52 -0300

    fix cors block + att diagramas

--- codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/controller/taskController.java
+++ codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/controller/taskController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class taskController {
 
